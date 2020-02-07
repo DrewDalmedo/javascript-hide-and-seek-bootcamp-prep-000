d9ffe8aa05969d62b264565d6977fa53a195a400
Andrew Dalmedo <andrewdalmedo@gmail.com>
2020-02-07 01:31:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
+// returns the first element that matches the selector
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+// pulls a .target out of #nested
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+// increases ranks in .ranked-list by n
+function increaseRankBy(n) {
+  let ranks = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < ranks.length; i++) {
+    ranks[i].innerHTML = (parseInt(ranks[i].innerHTML) + n).toString();
+  }
+}
+
+// returns the most deeply nested child in #grand-node
+function deepestChild() {
+  let grandNode = document.querySelector('#grand-node');
+  let current = grandNode;
+  let next = [];
+  
+  while (true) {
+    if (current.innerHTML !== null) {
+      console.log(current);
+      return current;
+    }
+    else {
+      for (let i = 0; i < current.length; i++) {
+        next.push(current.querySelector('div'));
+      }
+    }
+    
+    current = next.shift();
+  }
+  
+}
